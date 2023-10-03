2b4d5450439e504df7ef0d28271cb3d07e755017
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2023-10-03 16:02:52 +0200

    Lesson1: Refactor to use separate function for translated greeting

--- lesson_1_hello_world/src/main.rs
+++ lesson_1_hello_world/src/main.rs
@@ -1,24 +1,20 @@
 use std::env;
 
 fn main() {
-    let locale_greet = env::var("LANG")
-        .ok()
-        .and_then(|lang| {
-            lang.split('_')
-                .next()
-                .and_then(|lang|
-                          match lang {
-                              "en" => Some("Hello, world!"),
-                              "cs" => Some("Ahoj světe!"),
-                              "es" => Some("¡Hola el mundo!"),
-                              _ => None,
-                          }
-                         )}
-                );
-    if let Some(translated) = locale_greet {
+    if let Some(translated) = get_locale_greet() {
         println!("{}", translated);
     } else {
         println!("Hello, world!");
     }
+}
 
+fn get_locale_greet() -> Option<&'static str> {
+    env::var("LANG").ok().and_then(|lang| {
+        lang.split('_').next().and_then(|lang| match lang {
+            "en" => Some("Hello, world!"),
+            "cs" => Some("Ahoj světe!"),
+            "es" => Some("¡Hola el mundo!"),
+            _ => None,
+        })
+    })
 }
