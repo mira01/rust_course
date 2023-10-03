c5c99a36ae1126b6266f73826e6eea81060aa8f7
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2023-10-03 15:58:34 +0200

    Lesson1: Implement greeting based on locale

--- lesson_1_hello_world/src/main.rs
+++ lesson_1_hello_world/src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
 fn main() {
-    println!("Hello, Rust!");
+    let locale_greet = env::var("LANG")
+        .ok()
+        .and_then(|lang| {
+            lang.split('_')
+                .next()
+                .and_then(|lang|
+                          match lang {
+                              "en" => Some("Hello, world!"),
+                              "cs" => Some("Ahoj světe!"),
+                              "es" => Some("¡Hola el mundo!"),
+                              _ => None,
+                          }
+                         )}
+                );
+    if let Some(translated) = locale_greet {
+        println!("{}", translated);
+    } else {
+        println!("Hello, world!");
+    }
+
 }
