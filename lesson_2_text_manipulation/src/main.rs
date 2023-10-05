152a4e8da0166b5388fc6ddc912e1203eb4752d1
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2023-10-05 16:33:17 +0200

    Lesson2: Introduce unit test

--- lesson_2_text_manipulation/src/main.rs
+++ lesson_2_text_manipulation/src/main.rs
@@ -14,7 +14,8 @@ fn main() {
                     "uppercase",
                     "no-spaces",
                     "slugify",
-                    "little-big"
+                    "little-big",
+                    "camel-case"
                 ]
             );
             process::exit(1);
@@ -33,13 +34,14 @@ fn main() {
 }
 
 // :( I cannot write a function that dispatches closures based on HashMap key yet :)
-fn mutate(arg: &str, content: String) -> String {
+pub fn mutate(arg: &str, content: String) -> String {
     match arg {
         "lowercase" => content.to_lowercase(),
-        "upercase" => content.to_uppercase(),
+        "uppercase" => content.to_uppercase(),
         "no-spaces" => content.replace([' ', '\n'], ""),
         "slugify" => slugify(content),
         "little-big" => little_big(content),
+        "camel-case" => camel_case(content),
         _ => content,
     }
 }
@@ -57,5 +59,60 @@ fn little_big(content: String) -> String {
             word.push(' ');
             word
         })
+        .collect::<String>()
+        .trim()
+        .to_string()
+}
+
+// maybe could be simplified
+fn camel_case(content: String) -> String {
+    content
+        .split_whitespace()
+        .map(|w| {
+            let mut word = String::new();
+            if let Some(first) = w.chars().next() {
+                word = first.to_uppercase().to_string();
+                let rest: String = w
+                    .chars()
+                    .skip(1)
+                    .map(|ch| ch.to_lowercase().to_string())
+                    .collect();
+                word.push_str(&rest);
+            }
+            word
+        })
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::mutate;
+
+    #[test]
+    fn transformations() {
+        assert_eq!(
+            mutate("lowercase", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "lorem ipsum dolor sit amet"
+        );
+        assert_eq!(
+            mutate("uppercase", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "LOREM IPSUM DOLOR SIT AMET"
+        );
+        assert_eq!(
+            mutate("no-spaces", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "LoremipsumDOLORsITAmeT"
+        );
+        assert_eq!(
+            mutate("slugify", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "lorem-ipsum-dolor-sit-amet"
+        );
+        assert_eq!(
+            mutate("little-big", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "lorem IPSUM dolor SIT amet"
+        );
+        assert_eq!(
+            mutate("camel-case", "Lorem ipsum DOLOR sIT AmeT".to_string()),
+            "LoremIpsumDolorSitAmet"
+        );
+    }
 }
