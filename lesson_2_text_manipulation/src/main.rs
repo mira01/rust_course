f78721503940025ab75c1d00f239df0cb4e5ab12
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2023-10-27 20:38:17 +0200

    Select mode based on arguments

--- lesson_2_text_manipulation/src/main.rs
+++ lesson_2_text_manipulation/src/main.rs
@@ -3,23 +3,43 @@ use std::error::Error;
 use std::process;
 use transmuter::{Mutation, StringResult};
 
+#[derive(Clone, Copy)]
+enum Mode{
+    Interactive,
+    NonInteractive(Mutation),
+}
+
 fn main() {
-    match run() {
-        Ok(output) => print!("{}", output),
+    let result = get_mode()
+        .and_then(|mode|run(&mode));
+    match result {
+        Ok(result) => println!("{}", result),
         Err(error) => {
             eprintln!("\x1b[0;31m{}\x1b[0m", error);
             process::exit(1);
-        }
-    };
+        },
+    }
 }
 
-fn run() -> StringResult {
-    let mutation = get_mutation()?;
-    eprintln!("Will apply {}:", mutation);
-    mutation.mutate()
+fn run(mode: &Mode) -> StringResult {
+    match mode {
+        Mode::NonInteractive(mutation) => {
+            eprintln!("Will apply {}:", mutation);
+            mutation.mutate()
+        },
+        Mode::Interactive => enter_loop()
+    }
 }
 
-fn get_mutation() -> Result<Mutation, Box<dyn Error>> {
-    let mutation = env::args().nth(1).ok_or("Cli argument not provided")?;
-    Mutation::try_from(&mutation as &str)
+fn enter_loop() -> StringResult {
+    unimplemented!();
+}
+
+fn get_mode() -> Result<Mode, Box<dyn Error>> {
+    if env::args().len() == 1{
+        Ok(Mode::Interactive)
+    } else {
+        let mutation = env::args().nth(1).ok_or("Error reading mutation")?;
+        Ok(Mode::NonInteractive(Mutation::try_from(&mutation as &str)?))
+    }
 }
