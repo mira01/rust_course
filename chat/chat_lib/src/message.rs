05870cb275c7ae8a9761fb0e1855f141aaf7e5ad
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2023-12-03 17:11:57 +0100

    Use thiserror and anyhow for error handling

--- chat/chat_lib/src/message.rs
+++ chat/chat_lib/src/message.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 use bincode;
-use std::error::Error;
 use std::io::{Read, Write};
+
+use thiserror::Error;
 
 /// Supported messages in chat application
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -11,40 +12,63 @@ pub enum Message {
     Image(Vec<u8>),
 }
 
+#[derive(Error, Debug)]
+pub enum ChatLibError {
+    #[error("Cannot compose the message")]
+    ComposeError,
+    #[error("Cannot send the message")]
+    SendError,
+    #[error("Cannot receive the message")]
+    ReadError,
+}
+
 impl Message {
 
     /// Method for message serialization. If one wants to write the message via I/O,
     /// have a look at [write_to_stream] method.
-    pub fn serialize(&self) -> Result<Vec<u8>, Box<dyn Error>> {
-        Ok(bincode::serialize(&self)?.to_vec())
+    pub fn serialize(&self) -> Result<Vec<u8>, ChatLibError> {
+        Ok(bincode::serialize(&self)
+           .map_err(|_e| ChatLibError::ComposeError)?
+           .to_vec()
+          )
     }
 
     /// Method for writing a message trough I/O (File, Network, ...). Serialize message and writes
     /// its size and message itself into *Write*abale stream given as parameter
-    pub fn write_to_stream<T: Write>(&self, stream: &mut T) -> Result<(), Box<dyn Error>> {
+    pub fn write_to_stream<T: Write>(&self, stream: &mut T) -> Result<(), ChatLibError> {
         let serialized = self.serialize()?;
         let len = serialized.len() as u32;
-        stream.write_all(&len.to_be_bytes())?;
-        Ok(stream.write_all(&serialized)?)
+        stream.write_all(&len.to_be_bytes())
+            .map_err(|_e| ChatLibError::SendError )?;
+        Ok(stream.write_all(&serialized)
+           .map_err(|_e| ChatLibError::SendError)?)
     }
 
     /// Method for obtaining a message from *Read*able (File, Nework, ...)
-    pub fn read_from_stream<T: Read>(stream: &mut T) -> Result<Message, Box<dyn Error>> {
+    pub fn read_from_stream<T: Read>(stream: &mut T) -> Result<Message, ChatLibError> {
         let mut len_bytes = [0u8; 4];
-        stream.read_exact(&mut len_bytes)?;
+        stream.read_exact(&mut len_bytes)
+            .map_err(|_e| ChatLibError:: ReadError )?;
         let len = u32::from_be_bytes(len_bytes) as usize;
 
         let mut buffer = vec![0u8; len];
-        stream.read_exact(&mut buffer)?;
+        stream.read_exact(&mut buffer)
+            .map_err(|_e| ChatLibError:: ReadError )?;
 
         Message::try_from(buffer.as_slice())
     }
 }
 
 impl TryFrom<&[u8]> for Message {
-    type Error = Box<dyn Error>;
+    type Error = ChatLibError;
 
     fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
-        Ok(bincode::deserialize(data)?)
+        // You can create intentional error by sending "cus" message
+        if data == &vec![0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 99, 117, 115]{
+            return Err(ChatLibError::ComposeError);
+        }
+        Ok(bincode::deserialize(data)
+           .map_err(|_e| ChatLibError::ComposeError)?
+          )
     }
 }
--- chat/chat_server/src/main.rs
+++ chat/chat_server/src/main.rs
@@ -2,28 +2,29 @@
 
 use std::collections::HashMap;
 use std::env;
-use std::error::Error;
 use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
 use std::sync::mpsc;
-use std::thread;
+use std::thread::{self, JoinHandle};
 
 use threadpool::ThreadPool;
-use log::{Level, info, error};
+use log::{Level, info, error, warn};
 use stderrlog;
+use anyhow::{Result, Context, Error, bail};
 
-use chat_lib::message::Message;
+use chat_lib::message::{Message, ChatLibError};
 
 const DEFAULT_ADDRESS: &str = "127.0.0.1:11111";
 const THREAD_COUNT: usize = 8;
 
-fn main() {
+fn main() -> Result<()> {
     stderrlog::new()
         .verbosity(Level::Info)
-        .init()
-        .unwrap();
-    if let Err(e) = run() {
-        error!("{}", e);
-    }
+        .init()?;
+    run()
+        .map_err(|e| {
+            error!("{}", e);
+            e.context("Server crashed")
+        })
 }
 
 /// Structure that encapsulates client identification, stream and an event that happened
@@ -39,28 +40,33 @@ struct StoredData {
 #[derive(Debug)]
 enum Event {
     Message(Message),
+    ClientError(Error),
     Connected,
     Disconnected,
 }
 
 /// All logic happens here:
-fn run() -> Result<(), Box<dyn Error>> {
-    let host_port = env::args().nth(1).unwrap_or(DEFAULT_ADDRESS.into());
+fn run() -> Result<()> {
+    let host_port = env::args()
+        .nth(1)
+        .unwrap_or(DEFAULT_ADDRESS.into());
 
-    let tcp = TcpListener::bind(host_port)?;
+    let tcp = TcpListener::bind(&host_port)
+        .with_context(|| format!("Cannot listen on {}", &host_port))?;
     let pool = ThreadPool::new(THREAD_COUNT);
     let (tx, rx) = mpsc::channel::<StoredData>();
     let (responses_out, responses_in) = mpsc::channel::<(Message, TcpStream)>();
 
     // Thread that writes messages to clients' streams
-    let _responder = thread::spawn(move || {
+    let _responder: JoinHandle<Result<()>> = thread::spawn(move || {
         while let Ok((msg, mut stream)) = responses_in.recv() {
-            msg.write_to_stream(&mut stream).unwrap();
+            msg.write_to_stream(&mut stream)?;
         }
+        Ok(())
     });
 
     // Thread that keeps clients' connections and acts on incoming messages
-    let _processor = thread::spawn(move || {
+    let _processor: JoinHandle<Result<()>> = thread::spawn(move || {
         let mut clients: HashMap<SocketAddr, TcpStream> = HashMap::new();
         while let Ok(data) = rx.recv() {
             match data.event {
@@ -68,9 +74,7 @@ fn run() -> Result<(), Box<dyn Error>> {
                     for (address, stream) in &clients {
                         if address != &data.address {  // do not send to the author
                             info!("sending a message");
-                            responses_out
-                                .send((message.clone(), stream.try_clone().unwrap()))
-                                .unwrap();
+                            responses_out.send((message.clone(), stream.try_clone()?))?;
                         }
                     }
                 }
@@ -82,50 +86,63 @@ fn run() -> Result<(), Box<dyn Error>> {
                     info!("a client conected");
                     clients.insert(data.address, data.stream);
                 }
+                Event::ClientError(e) => {
+                    warn!("client error {}", e);
+                    responses_out
+                        .send((Message::Text(format!("You caused an error on server {}", e)), data.stream.try_clone()?))?;
+                }
             }
         }
+        Ok(())
     });
+
+    /// Function for acting upon incoming message
+    fn handle_stream(stream: std::io::Result<TcpStream>, tx: mpsc::Sender<StoredData>) -> Result<()> {
+        let mut stream = stream.context("Cannot read from data stream")?;
+        let address = stream.peer_addr().context("Cannot get peer addres")?;
+        let tx = tx.clone();
+
+        let data = StoredData {
+            event: Event::Connected,
+            address,
+            stream: stream.try_clone()?,
+        };
+
+        tx.send(data)?;
+        loop {
+            let message = Message::read_from_stream(&mut stream);
+            let event_type = match message {
+                Ok(message) => Event::Message(message),
+                Err(e @ ChatLibError::ComposeError) => Event::ClientError(e.into()),
+                Err(_) => Event::Disconnected,
+            };
+            let mut should_exit = false;
+            if let Event::Disconnected = event_type {
+                should_exit = true;
+            }
+            let data = StoredData {
+                event: event_type,
+                address,
+                stream: stream.try_clone()?,
+            };
+            tx.send(data)?;
+            if should_exit {
+                let _ = stream.shutdown(Shutdown::Both);
+                break Ok(());
+            }
+        }
+    }
+
 
     // Read data from tcp and handle them in thread pool
     for stream in tcp.incoming() {
-        let tx = tx.clone();
+        let tx2 = tx.clone();
         pool.execute(move || {
-            let mut stream = stream.unwrap();
-            let address = stream.peer_addr().unwrap();
-            let tx = tx.clone();
-
-            let data = StoredData {
-                event: Event::Connected,
-                address,
-                stream: stream.try_clone().unwrap(),
-            };
-
-            tx.send(data).unwrap();
-            loop {
-                let message = Message::read_from_stream(&mut stream);
-                match message {
-                    Ok(message) => {
-                        let data = StoredData {
-                            event: Event::Message(message),
-                            address,
-                            stream: stream.try_clone().unwrap(),
-                        };
-                        tx.send(data).unwrap();
-                    }
-                    Err(_) => {
-                        let _ = stream.shutdown(Shutdown::Both);
-                        let data = StoredData {
-                            event: Event::Disconnected,
-                            address,
-                            stream: stream.try_clone().unwrap(),
-                        };
-                        tx.send(data).unwrap();
-                        break;
-                    }
-                }
+            match handle_stream(stream, tx2) {
+                Ok(_) => info!("done processing stream"),
+                Err(e) => error!("failed processing tcp stream {}", e), 
             }
-        });
+        } );
     }
-
     Ok(())
 }
